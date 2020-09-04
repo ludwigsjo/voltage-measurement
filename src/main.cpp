b2560511cbcf7fb83b59013a3f33331d37ef5ca2
Ludwig Sjöberg <ludwig.sjoberg@skola.botkyrka.se>
2020-09-04 15:01:58 +0200

    Green LED light when voltage over 1.5 and red LED light when under 1.5

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,23 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int value = analogRead(A0);
+  float voltage =5.0 * value / 1024;
+  Serial.println(voltage);
+  delay(100);
+
+  if (voltage > 1.5) {
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+  } else {
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+  }
 }
